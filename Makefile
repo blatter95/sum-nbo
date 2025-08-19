16ffb6eac6e6473bb83e06a3f6d28fe2ea40b8aa
blatter95 <gomwer678@gmail.com>
2025-08-20 03:44:02 +0900

    Update sum-nbo.cpp, Makefile

--- Makefile
+++ Makefile
@@ -1,11 +1,6 @@
 #Makefile
 all: sum-nbo
 
-binfile: 
-	echo -n -e \\x00\\x00\\x03\\xe8 > thousand.bin
-	echo -n -e \\x00\\x00\\x01\\xf4 > five-hundred.bin
-	echo -n -e \\x00\\x00\\x00\\xc8 > two-hundred.bin
-
 sum-nbo: sum-nbo.o
 	g++ -o sum-nbo sum-nbo.o
 
@@ -13,6 +8,5 @@ sum-nbo.o: sum-nbo.cpp
 	g++ -c -o sum-nbo.o sum-nbo.cpp
 
 clean:
-	rm -f sum-test
+	rm -f sum-nbo
 	rm -f *.o
-	rm -f *.bin
---
+++ sum-nbo.cpp
@@ -0,0 +1,36 @@
+#include <stddef.h> // for size_t
+#include <stdint.h> // for uint8_t
+#include <stdio.h> // for printf
+#include <netinet/in.h> // for htons, htonl
+
+int main(int argc, char* argv[]) {
+    uint32_t num;
+    uint32_t total = 0;
+    FILE *fp;
+
+    for (int i = 1; i < argc; i++) {
+        // file open
+        fp = fopen(argv[i], "rb");
+        if (fp == NULL) {
+            printf("Error: Could not file open: %s\n", argv[i]);
+            return 1;
+        }
+
+        // 4byte check
+        if (fread(&num, sizeof(num), 1, fp) != 1) {
+            printf("Error: Could not 4bytes from file: %s\n", argv[i]);
+            fclose(fp);
+            return 1;
+        }
+
+        // change byte order
+        uint32_t host_num = ntohl(num);
+        total += host_num;
+
+        // print
+        if (i > 1) { printf(" + "); }
+        printf("%u(0x%08x)", host_num, host_num);
+        fclose(fp);
+    }
+    printf(" = %u(0x%08x)\n", total, total);
+}
